83fae6adb1cee516c68e6f64a1b6c6b2a5bc902a
Hakan Erikli <hakanerikli@gmail.com>
2023-07-26 15:56:56 +0300

    turtle square code added.

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import turtle
 drawing_board = turtle.Screen()
 drawing_board.bgcolor("green")
 drawing_board.title("Python Turtle")
-turtle_instance = turtle.Turtle()
 
+#create a square
+turtle_instance = turtle.Turtle()
 turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+
 turtle.done()
