84092e29249ed8af493097e331728f4c177bd318
Hakan Erikli <hakanerikli@gmail.com>
2023-07-26 17:25:08 +0300

    star drawing added.

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ drawing_board.title("Python Turtle")
 
 #create a square
 turtle_instance = turtle.Turtle()
+'''
 turtle_instance.forward(100)
 turtle_instance.left(90)
 turtle_instance.forward(100)
 turtle_instance.left(90)
 turtle_instance.forward(100)
 turtle_instance.left(90)
-turtle_instance.forward(100)
+turtle_instance.forward(100)'''
+'''for i in range(4):
+    turtle_instance.left(90)
+    turtle_instance.forward(100)
+'''
+
+#drawing a star
+for i in range(5):
+    turtle_instance.left(72)
+    turtle_instance.forward(100)
+    turtle_instance.right(144)
+    turtle_instance.forward(100)
 
 turtle.done()
