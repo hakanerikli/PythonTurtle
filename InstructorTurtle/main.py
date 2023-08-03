b27803c740773499602f9cfeee28e4aa0fa2b856
Hakan Erikli <hakanerikli@gmail.com>
2023-08-03 16:37:20 +0300

    instructor turtle revisioned.

--- InstructorTurtle/main.py
+++ InstructorTurtle/main.py
@@ -1,5 +1,12 @@
 import turtle
 
+myturtle= turtle.Turtle()
 screen=turtle.Screen()
 screen.bgcolor("light blue")
-screen.title("Instructor Turtle")
+screen.title("Instructor's Turtle")
+
+FONT = ("Verdana", 50, "normal")
+myturtle.setpos(0,200)
+myturtle.write("Test", align="center",font=FONT)
+
+turtle.mainloop()
