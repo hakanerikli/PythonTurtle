1d2cf2654a61cfe614878c5c5220faa4ed8fd925
Hakan Erikli <hakanerikli@gmail.com>
2023-08-04 11:14:55 +0300

    score turtle and countdown turtle are added.

--- InstructorTurtle/main.py
+++ InstructorTurtle/main.py
@@ -1,12 +1,25 @@
 import turtle
 
-myturtle= turtle.Turtle()
-screen=turtle.Screen()
+myturtle = turtle.Turtle()
+screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Instructor's Turtle")
-
 FONT = ("Verdana", 50, "normal")
-myturtle.setpos(0,200)
-myturtle.write("Test", align="center",font=FONT)
+
+# MyTurtle
+myturtle.penup()
+myturtle.setpos(0, 200)
+myturtle.write("Test", align="center", font=FONT)
+myturtle.hideturtle()
+
+# Score Turtle
+score_turtle = turtle.Turtle()
+score_turtle.color("dark blue")
+score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
+
+# Countdown Turtle
+countdown_turtle = turtle.Turtle()
+countdown_turtle.color("red")
+countdown_turtle.write(arg="Time: 0", move=False, align="center", font=FONT)
 
 turtle.mainloop()
